c817ea83aeccfdb08adc156f019c67f2a6b05d08
Mehdi El Akary <rock.mido15@gmail.com>
2024-10-15 18:01:31 +0100

    some improvement after eid al 7ad

--- srcs/map_check_2.c
+++ srcs/map_check_2.c
@@ -6,7 +6,7 @@
 /*   By: mel-akar <mel-akar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/12 15:33:26 by mel-akar          #+#    #+#             */
-/*   Updated: 2024/10/15 17:55:24 by mel-akar         ###   ########.fr       */
+/*   Updated: 2024/10/15 17:57:30 by mel-akar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,11 +19,9 @@ bool is_map(char *s)
 	i = -1;
 	if (!s)
 		return (true);
-	while (s[++i] /* && s[i] != '\n'*/)
+	while (s[++i] && s[i] != '\n')
 	{
-		if (s[i] == '\t')
-			ft_error(MAP_ERR, MAP_STT);
-		if (s[i] != ' ' && s[i] != '1' && s[i] != '\n')
+		if (s[i] != ' ' && s[i] != '1')
 			return (false);
 	}
 	return (true);
