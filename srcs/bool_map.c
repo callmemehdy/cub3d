56327912614c2488001580729ed7f37fba0a0c24
Mehdi El Akary <rock.mido15@gmail.com>
2024-10-18 11:51:08 +0100

    the beg of bool map handlin' hhhh

--- srcs/bool_map.c
+++ srcs/bool_map.c
@@ -6,7 +6,7 @@
 /*   By: mel-akar <mel-akar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/16 17:06:41 by mel-akar          #+#    #+#             */
-/*   Updated: 2024/10/16 19:09:02 by mel-akar         ###   ########.fr       */
+/*   Updated: 2024/10/17 15:11:10 by mel-akar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,7 @@ bool	bool_map(t_data *data)
 {
 	char	**map;
 	int		i;
+	int		j;
 
 	'v' && (map = data->map, i = -1);
 	if (!map)
@@ -24,6 +25,11 @@ bool	bool_map(t_data *data)
 		return (false);
 	while (*(map + ++i))
 	{
+		j = -1;
+		while (*(*(map + i) + ++j))
+		{
+			printf("%c", *(*(map + i) + j));
+		}
 		// check everyline's ['0', 'N', 'S', 'E', 'W']'s chars
 		// if they're surrounded by any space ... if they, error.		
 	}
