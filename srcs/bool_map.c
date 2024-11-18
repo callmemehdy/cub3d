23e3e84093cb6bcf10c1d6c9ed4097439039245f
Mehdi El Akary <rock.mido15@gmail.com>
2024-11-18 17:14:01 +0100

    fix a to byte function

--- srcs/bool_map.c
+++ srcs/bool_map.c
@@ -6,11 +6,46 @@
 /*   By: mel-akar <mel-akar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/16 17:06:41 by mel-akar          #+#    #+#             */
-/*   Updated: 2024/10/17 15:11:10 by mel-akar         ###   ########.fr       */
+/*   Updated: 2024/11/18 16:48:46 by mel-akar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cub3d.h"
+
+
+char	*rmjoin(char *s, size_t maxlen)
+{
+	size_t		len;
+	char		*buff;
+
+	len = ft_strlen(s);
+	len < maxlen && (len = maxlen);
+	buff = ft_malloc(sizeof(char) * len);
+	if (!buff)
+		ft_error(ALLOC_ERR, ALLOC_STT);
+	
+}
+
+size_t	longest_line(char **s)
+{
+	int		i;
+	size_t	maxlen;
+	size_t	len;
+
+	'?' && (maxlen = 0, i = -1);
+	while (s[++i])
+	{
+		len = ft_strlen(s[i]);
+		if (len > maxlen)
+			maxlen = len;
+	}
+	return (maxlen);
+}
+
+char	**to_rec(char **s)
+{
+	
+}
 
 bool	bool_map(t_data *data)
 {
@@ -23,6 +58,7 @@ bool	bool_map(t_data *data)
 		return (false);
 	if (!_edgelines(*map))
 		return (false);
+	map = to_rec(map);
 	while (*(map + ++i))
 	{
 		j = -1;
@@ -30,8 +66,9 @@ bool	bool_map(t_data *data)
 		{
 			printf("%c", *(*(map + i) + j));
 		}
+		printf("\n");
 		// check everyline's ['0', 'N', 'S', 'E', 'W']'s chars
-		// if they're surrounded by any space ... if they, error.		
+		// if they're surrounded by any space ... if they, error.
 	}
 	return (true);
 }
