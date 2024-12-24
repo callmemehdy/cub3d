67302c524cd152f43a8f730ee2d16a0c313024a3
Anas El Ammari <ael-amma@e2r8p3.1337.ma>
2024-12-24 19:01:33 +0100

    Cleaning the code

--- srcs/raycast/game.c
+++ srcs/raycast/game.c
@@ -6,7 +6,7 @@
 /*   By: ael-amma <ael-amma@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/12 15:35:24 by ael-amma          #+#    #+#             */
-/*   Updated: 2024/12/24 18:55:45 by ael-amma         ###   ########.fr       */
+/*   Updated: 2024/12/24 19:00:59 by ael-amma         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,6 @@ void	game(void)
 {
 	setup();
 	render_minimap();
-	printf("%f\n", M_PI);
 	mlx_image_to_window((*get_rdata())->mlx, (*get_rdata())->img, 0, 0);
 	mlx_loop((*get_rdata())->mlx);
 }
