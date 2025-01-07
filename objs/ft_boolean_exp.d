5d1f14b2f8bebff8abcc1025e1e662d3387ce714
callmemehdy <rock.mido15@gmail.com>
2025-01-07 09:04:26 +0100

    day 1347 of cub3d project ended

--- objs/ft_boolean_exp.d
+++
@@ -1,2 +0,0 @@
-objs/ft_boolean_exp.o: srcs/parse/ft_boolean_exp.c incs/cub3d.h \
- incs/get_next_line.h MLX42/include/MLX42/MLX42.h
--- objs/get_next_line.d
+++
@@ -1,1 +0,0 @@
-objs/get_next_line.o: srcs/parse/get_next_line.c incs/get_next_line.h
