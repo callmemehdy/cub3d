f6e30aae211f87d7b1cc19e932108a9aa8969521
Mehdi El Akary <rock.mido15@gmail.com>
2025-01-24 03:07:47 +0100

    added some remarks about the cursor system

--- cub3d_b/srcs/raycast/hooks_bonus.c
+++ cub3d_b/srcs/raycast/hooks_bonus.c
@@ -6,7 +6,7 @@
 /*   By: mel-akar <mel-akar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/25 14:59:27 by ael-amma          #+#    #+#             */
-/*   Updated: 2025/01/22 21:06:46 by mel-akar         ###   ########.fr       */
+/*   Updated: 2025/01/24 03:07:09 by mel-akar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,14 +14,34 @@
 
 static void	key_release(mlx_key_data_t keydata, t_mlx *mlx);
 
+int		curr_x;
+bool	first = false;
 
-// Mouse handler function matching MLX42's required signature
-void mouse_hdl(mouse_key_t key, action_t action, modifier_key_t mods, void* param)
+void mouse_hdl(double x, double y, void* param)
 {
-    if (action == MLX_PRESS)
-        printf("Mouse button %d pressed\n", key);
-    else if (action == MLX_RELEASE)
-        printf("Mouse button %d released\n", key);
+	t_mlx			*mlx;
+	int				padder;
+	int				win_x;
+
+	// make some var for the center of the visible portion of the window
+	1 && (mlx = *get_mlx(), padder = 0);
+	if (!first)
+	{
+		mlx_set_mouse_pos(mlx->mlxi, W_WIDTH / 2, W_HEIGHT / 2);
+		mlx_set_cursor_mode(mlx->mlxi, MLX_MOUSE_HIDDEN);
+		first = true;
+	}
+	else
+	{
+		mlx_get_window_pos(mlx->mlxi, &win_x, &padder);
+		if (curr_x > x)
+			mlx->player->angle -= 0.04;
+		else if (curr_x < x)
+			mlx->player->angle += 0.04;
+		curr_x = x;
+		if (x < W_WIDTH / 2 || x > W_WIDTH / 2 || win_x < 0)
+			mlx_set_mouse_pos(mlx->mlxi, W_WIDTH / 2, W_HEIGHT / 2);		
+	}
 }
 
 
@@ -30,7 +50,7 @@ void	key_press(mlx_key_data_t keydata, void *vmlx)
 	t_mlx	*mlx;
 
 	mlx = vmlx;
-	mlx_mouse_hook(mlx->mlxi, mouse_hdl, NULL);
+	mlx_cursor_hook(mlx->mlxi, mouse_hdl, NULL);
 	if (keydata.key == MLX_KEY_ESCAPE)
 		ft_exit(mlx);
 	if (keydata.action == MLX_PRESS || keydata.action == MLX_REPEAT)
